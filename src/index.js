becb84da665b6fae562a37787ce5e0b893392fec
Nik79 <niknikcor@gmail.com>
2018-03-02 16:44:42 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var currentStr = str;
+
+   for (var k = 0; k < bracketsConfig.length; k++)
+   {
+    var currentPair = bracketsConfig[k];
+    var patternForReplacement = currentPair[0] + currentPair[1];
+
+    for (var j = 0; j <= str.length/2; j++) {
+     currentStr = currentStr.replace(patternForReplacement, '');
+    }
+   }
+
+   var isCorrect = (currentStr.length == 0);
+
+   return isCorrect;
 }
